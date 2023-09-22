54389245abed88ef2c9509c1c27adc29c86ec801
martimalexander <120787261+martimalexander@users.noreply.github.com>
2023-09-22 17:33:48 +0530

    uff

--- config.py
+++ config.py
@@ -6,9 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","5796451094:AAFHHF9FsomhKCIu8uOY-UX8YGvN79-yYLY")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("OWNER")
-
+    API_ID = int(os.environ.get("API_ID","9907811"))
+    API_HASH = os.environ.get("API_HASH","b5adb7f7d4a096750edec1bc6daacd56")
+    AUTH_USERS = os.environ.get("OWNER","5149523544")
